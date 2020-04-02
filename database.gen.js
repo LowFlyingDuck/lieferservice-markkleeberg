b63fcd286b8cb0bd2745ef697ff65eab243da273
root <root@finn.finn>
2020-04-02 21:34:23 +0200

    first release

--- database.gen.js
+++ database.gen.js
@@ -1,1 +1,10 @@
-for (element of document.querySelector('.listing').children) if(element.nodeName === 'DIV') objection[element.children[0].children[1].children[2].textContent.trim()] = { img: element.children[0].children[1].children[0].children[0].children[0].children[0].srcset, prize: element.children[0].children[1].children[4].children[1].children[0].textContent.replace('*', '').trim(), comPrize:  parseFloat(element.children[0].children[1].children[4].children[1].children[0].textContent.replace(',', '.').trim())}
+var products = [];
+for (element of document.querySelector('.listing').children) if(element.nodeName === 'DIV') products.push({
+  name: element.children[0].children[1].children[2].textContent.trim(),
+  img: element.children[0].children[1].children[0].children[0].children[0].children[0].srcset, 
+  prize: element.children[0].children[1].children[4].children[1].children[0].textContent.replace('*', '').trim(), 
+  comPrize:  parseFloat(element.children[0].children[1].children[4].children[1].children[0].textContent.replace(',', '.').trim())
+});
+var textarea = document.createElement('TEXTAREA');
+document.body.append(textarea);
+textarea.value = JSON.stringify(products, 0, 2);
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // setup - server
+const state = 'development';
 const { exec } = require('child_process');
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const randomstring = require('randomstring');
 const redServer = require('http').createServer((req, res) => {
   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
   res.end();
@@ -12,6 +14,16 @@ const server = require('https').createServer({
   cert: fs.readFileSync('./fullchain.pem', 'utf-8')
 }, app);
 //setup - serverfunctions
+const transporter = require('nodemailer').createTransport({
+  host: 'smtp.strato.de',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'mail@lieferservice-markkleeberg.de',
+    pass: 'mail-arndt32xNX'
+  }
+});
+const email = 'carlinschilling@gmail.com';
 const compression = require('compression');
 const helmet = require('helmet');
 const database = JSON.parse(fs.readFileSync('./database.json'));
@@ -20,10 +32,11 @@ const database = JSON.parse(fs.readFileSync('./database.json'));
 app.use(compression());
 app.use(helmet());
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 
 // routes
 app.get('/impressum', (request, response) => {
-  response.sendFile('pubic/pages/impressum.html');
+  response.sendFile(__dirname + '/public/pages/impressum.html');
 })
 
 app.get('/retrieveCount', (request, response) => {
@@ -37,22 +50,39 @@ app.get('/retrieveDatabase', (request, response) => {
 app.get('/retrieveProducts', (request, response) => {
   var start = parseInt(request.query.start);
   var end = parseInt(request.query.end);
+  var search = (request.query.search || '').toLowerCase();
+  var filtered = [];
   var output = '';
-  for (var i = start; i < end && i < database.products.length; i++) {
-    var path = database.products[i];
-    output += makeProduct(path.name, path.img, path.prize, path.comPrize);
+  for (let product of database.products) {
+    if (product.name.toLowerCase().includes(search)) filtered.push(product);
   }
+  for (var i = start; i < end && i < filtered.length; i++) output += makeProduct(filtered[i].name, filtered[i].img, filtered[i].prize, filtered[i].comPrize)
+  response.setHeader('app-itemcount', filtered.length.toString());
   response.send(output);
 })
 
 function makeProduct(name, img, prize, comPrize) {
-  var item = `<div class="product" prize="${prize}" comPrize="${comPrize}">
+  var id = randomstring.generate(12);
+  var item = `<div class="product" id="${id}">
     <img srcset="${img}" />
     <p class="name">${name}</p>
-    <p class="prize">${prize}</p>
+    <p class="prize">${prize} <span onclick="javascript: entry('${id}','${name}','${img}','${prize}','${comPrize}')">+</span></p>
   </div>`
   return item;
 }
+
+app.post('/placeOrder', (request, response) => {
+  products = '';
+  for (let option of JSON.parse(request.body)) {
+    products += `<div><h2>${option.name}</h2><p></p></div>`
+  }
+  var insertion = '<div><h1>Eine neue Bestellung wurde aufgegeben</h1><p>Bestelldetails</p></div>';
+  transporter.sendMail({
+    to: 'nils.schwebel.privat@gmail.com',
+    from: '"Lieferservice Markkleeberg" <mail@lieferservice-markkleeberg.de',
+    html: '<div><h1>Eine neue Bestellung wurde aufgegeben</h1><p>Bestelldetails</p></div>'
+  });
+})
 
 setInterval(() => exec('sudo certbot renew', (err, stdout) => {
   console.log(stdout);
